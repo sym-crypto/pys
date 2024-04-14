8e1ee26d9f35cf2f154b9c9dbbed2b8263ce62d5
sym-crypto <78688194+sym-crypto@users.noreply.github.com>
2024-04-14 17:41:45 +0200

    Update pys.cmd

--- Windows/pys.cmd
+++ Windows/pys.cmd
@@ -1,3 +1,21 @@
 echo off
 title PYS
 cls
+
+set sd=C:\Users
+set apphome=%sd%\%USERNAME%\pys
+set dsrv=%sd%\%USERNAME%\www\scarica
+
+
+cls
+type %apphome%\title-pys
+echo python3 web server
+echo ...
+echo all ip...
+ipconfig | findstr IPv4
+echo wlan:
+netsh wlan show all | findstr Driver
+C:
+cd %dsrv%
+py -m http.server 1212 | echo server_online http://0.0.0.0:1212 ... %dsrv%
+pause
